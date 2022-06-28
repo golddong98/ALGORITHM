5b5e2bf7d8dc6659d58c1c59654a91cac76f3a3e
golddong98 <73287240+golddong98@users.noreply.github.com>
2022-06-28 16:19:52 +0900

    [solving]: 내일 다시해보자

---
+++ BAEKJOON/분류별/자료구조/1874.py
@@ -0,0 +1,33 @@
+# # 출처: https://www.acmicpc.net/problem/1874
+
+# from sys import stdin
+
+# n = int(stdin.readline())
+
+# stack = []
+
+# for i in range(n+1,1, -1):
+#     if not stack:
+#         stack.push(i)
+#         print("+")
+
+#     else:
+#         k = stack[-1]
+
+#         x = int(stdin.readline())
+
+#         if k == x:
+#             stack.pop()
+#             print("-")
+
+
+# def stackFunc(stack, x):
+    
+
+
+
+
+
+
+
+
