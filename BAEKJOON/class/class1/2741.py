0afe84a6c0dfb96bafd6e4d79068ecc6019d5fdb
golddong98 <73287240+golddong98@users.noreply.github.com>
2022-12-11 18:39:03 +0900

    solve: N 찍기

---
+++ BAEKJOON/class/class1/2741.py
@@ -0,0 +1,21 @@
+# 출처: https://www.acmicpc.net/problem/2741
+
+# n = int(input())
+# i = 1
+# while i <= n:
+#     print(i)
+#     i+=1
+
+from sys import stdout
+
+print=stdout.write
+
+for i in range(int(input())):
+    print(str(i+1))
+
+
+
+
+
+
+    
