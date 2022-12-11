3e8265630165c5922395a50e29e7c41ca56d6e71
golddong98 <73287240+golddong98@users.noreply.github.com>
2022-12-11 16:54:33 +0900

    solve: 별찍기-1

---
+++ BAEKJOON/class/class1/2438.py
@@ -0,0 +1,14 @@
+# 출처: https://www.acmicpc.net/problem/2438
+
+n = int(input())
+
+# i = 1
+
+# while i <= n:
+#     print('*'*i)
+#     i+=1
+
+for i in range(2, n):
+    print(i)
+
+# print('*'*n)
