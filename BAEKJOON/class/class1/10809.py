ba1c6c10bd5e799c6f79594a9f414c5125b5b794
golddong98 <73287240+golddong98@users.noreply.github.com>
2022-12-15 23:12:58 +0900

    solve:알파벳 찾기

---
+++ BAEKJOON/class/class1/10809.py
@@ -0,0 +1,29 @@
+# 출처: https://www.acmicpc.net/problem/10809
+
+# alphabet = [-1]*26
+
+# word = input()
+
+# for i in range(len(word)):
+#     for a in range(97,123):
+#         if word[i] == chr(a) and alphabet[a-97] == -1:
+#             alphabet[a-97] = i
+#             break
+            
+    
+
+# print(*alphabet)
+
+
+
+# symbol = 'abcdefghijklmnopqrstuvwxyz'
+# a = input()
+# for i in symbol:
+#     ind = a.find(i)
+#     print(ind, end=' ')
+
+
+
+
+
+
