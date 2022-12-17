a88015ee3416af503f8664ee303d111a792afe6b
golddong98 <73287240+golddong98@users.noreply.github.com>
2022-12-17 18:37:56 +0900

    solve:최소, 최대

---
+++ BAEKJOON/class/class1/10818.py
@@ -0,0 +1,30 @@
+# 출처: https://www.acmicpc.net/problem/10818
+
+# i = 0
+# n = int(input())
+
+# numbers = list(map(int, input().split()))
+
+# mi = 1000000
+# ma = -1000000
+
+# while i<n:
+#     new_n = numbers[i]
+#     if new_n < mi:
+#         mi = new_n
+#     if new_n > ma:
+#         ma = new_n
+#     i+=1
+
+
+# print(mi, ma)
+
+# import sys 
+
+# _, *a = map(int, sys.stdin.buffer.read().split())
+
+# print(min(a), max(a))
+
+# import sys
+# _, *A = map(int, sys.stdin.buffer.read().split())
+# print(min(A), max(A))
