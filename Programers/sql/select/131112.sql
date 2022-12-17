1e863eb7dfe3007124fe0af67be3303b7a04497d
golddong98 <73287240+golddong98@users.noreply.github.com>
2022-12-17 20:15:06 +0900

    solve:강원도에 위치한 생산공장 목록 출력하기

---
+++ Programers/sql/select/131112.sql
@@ -0,0 +1,4 @@
+-- 출처:https://school.programmers.co.kr/learn/courses/30/lessons/131112
+
+SELECT FACTORY_ID, FACTORY_NAME, ADDRESS FROM FOOD_FACTORY WHERE ADDRESS LIKE '%강원도%' ORDER BY FACTORY_ID ASC
+
